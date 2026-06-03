8d985e54e60b3d68ccf59ad03c0e8901829b785f
Kiarad <kiashadow2007@gmail.com>
2026-06-03 02:21:54 -0400

    start

---
+++ cube.c
@@ -0,0 +1,19 @@
+#include <math.h>
+
+float A, B, C;
+
+float calculateX(int i, int j, int k){
+  
+}
+
+float calculateY(int i, int j, int k){
+  
+}
+
+float calculateZ(int i, int j, int k){
+  
+}
+
+int main(){
+  return 0; 
+}
