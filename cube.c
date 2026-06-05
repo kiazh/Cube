77dbea98767a23ccc006c7da6096611ac36f823a
Kiarad <kiashadow2007@gmail.com>
2026-06-04 22:44:54 -0400

    testing cmd comands to clear screen and get text to be at a fixed location

--- cube.c
+++ cube.c
@@ -1,19 +1,37 @@
 #include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <stdint.h>
+
+#define NB_COLS 206
+#define NB_ROWS 49
+
+uint8_t screen[NB_ROWS][NB_COLS];
 
 float A, B, C;
 
 float calculateX(int i, int j, int k){
-  
+  return 0.0f; 
 }
 
 float calculateY(int i, int j, int k){
-  
+  return 0.0f;
 }
 
 float calculateZ(int i, int j, int k){
-  
+  return 0.0f;
 }
 
 int main(){
-  return 0; 
+  memset(screen, ' ', sizeof(screen));
+  
+  printf("\x1B[2J\x1B[?25l");
+  while(true){
+    printf("\x1B[Hhello world!");
+  }
+  printf("\x1B[2J\x1B[?25h");
+
+  return 0;
 }
